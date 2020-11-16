7641ad83e57fd679efc1877186a7ab65a6c2a96b
Jim Peterson <omesadev@outlook.com>
2020-11-16 06:22:54 -0500

    travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://blogster:pJ62xOwYAz02AIuL@blogster-shard-00-00.lqnxf.mongodb.net:27017,blogster-shard-00-01.lqnxf.mongodb.net:27017,blogster-shard-00-02.lqnxf.mongodb.net:27017/blogster?ssl=true&replicaSet=atlas-dv67on-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
---
+++ tests/setup.js
@@ -0,0 +1,10 @@
+// set jest timeout to be 30 seconds
+jest.setTimeout(30000)
+
+require('../models/User')
+
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+
+mongoose.Promise = global.Promise
+mongoose.connect(keys.mongoURI, { useMongoClient: true })
